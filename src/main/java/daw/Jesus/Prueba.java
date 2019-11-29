69465e2c1a8d85647dc6235fd0b458b01ff8a9b7
jesusproint998 <jesusproint998@gmail.com>
2019-11-29 18:43:31 +0100

    Crea un objeto Jugador

--- src/main/java/daw/Jesus/Prueba.java
+++ src/main/java/daw/Jesus/Prueba.java
@@ -14,6 +14,8 @@ public class Prueba {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo");
+        
+        Jugador j= new Jugador("Jesus", 10, 1001);
     }
     
 }
